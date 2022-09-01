5497b911772e4a09a62c52fa63068b39c788dac2
kemal <ik@frontier.kz>
2022-09-01 11:20:33 +0600

    reset form fields after cancel

--- src/pages/Users/UsersContainer.tsx
+++ src/pages/Users/UsersContainer.tsx
@@ -31,6 +31,7 @@ export const UsersContainer = () => {
     const onCancel = () => {
         setCreateUserModal(null)
         setUpdateUserModal(null)
+        setChangesFormData(initialFormData)
     }
 
     const onOpenCreateUpdateUserModal = (type: string, value = '') => {
